96223dc09802c539f91b9124078ed8ee8767e807
Kenezke1 <herczeg.kenya2@gmail.com>
2018-01-22 14:09:17 +0100

    added attributes to hores

--- src/codecool/Horses.java
+++ src/codecool/Horses.java
@@ -6,16 +6,23 @@ public class Horses{
     int maxSpeed;
     int acceleration;
     int stamina;
+    boolean tired;
+    int tiredSpeed;
 
-    Horses(String name, int maxSpeed, int acceleration, int stamina){
+    Horses(String name, int maxSpeed, int acceleration, int stamina,int tiredSpeed){
         this.name = name;
         this.maxSpeed = maxSpeed;
         this.acceleration = acceleration;
         this.stamina = stamina;
+        this.tired = false;
+        this.tiredSpeed = tiredSpeed;
         horseCounter++;
     }
     public int getHorseCounter() {
         return horseCounter;
+    }
+    public  void setTired(){
+        this.tired = true;
     }
     
 }
