52ac7a1112856532cb54c995d9e1f0bf39285ded
Kenezke1 <herczeg.kenya2@gmail.com>
2018-01-22 13:26:26 +0100

    date formatting

--- src/codecool/Main.java
+++ src/codecool/Main.java
@@ -1,9 +1,11 @@
 package codecool;
 import codecool.Horses;
-
+import java.util.Date;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 
 public class Main{
-
+    private static final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
     Horses[] horses = new Horses[Horses.horseCounter];
 
     public Horses[] generateSimulation(int round){
@@ -13,6 +15,13 @@ public class Main{
 
 public static void main(String[] args){
     Main x = new Main();
-    System.out.println(x.horses.length);
+    Date d = new Date();
+    System.out.println(d);
+
+    Timestamp timeStamp = new Timestamp(System.currentTimeMillis());
+    System.out.println(timeStamp);
+
+    System.out.println(sdf.format(timeStamp));
+    
     }
 }
